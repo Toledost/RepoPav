7d7d76bba96437621596a03d6ddddf3d969fa45b
Federico Ayme <federico.ayme@gmail.com>
2020-08-10 15:59:10 -0300

    comentario linea 11

--- TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
+++ TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
@@ -8,16 +8,17 @@ namespace TESTWF2020.Clases
 {
     static class DatosConexion
     {
+        // este es un comentario para explicar que estas son las cadenas de conexion de cada uno
         //Federico
         //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
 
         //Lautaro
         //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
 
-        ////Bruno
-        public static string connectionString = @"Data Source=DESKTOP-0C4GOB8\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
+        //Bruno
+        //public static string connectionString = @"Data Source=DESKTOP-0C4GOB8\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
 
-        ////Martin
-        public static string connectionString = @"Data Source=DESKTOP-J8IAG5M\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
+        //Martin
+        //public static string connectionString = @"Data Source=DESKTOP-J8IAG5M\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
     }
 }
