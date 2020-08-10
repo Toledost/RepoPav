a59cf0b33a53d71a314a2cda1e9ab3b2b1c6ee77
Federico Ayme <federico.ayme@gmail.com>
2020-08-10 11:46:20 -0300

    agregada clase de cadena de conexion

---
+++ TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTWF2020.Clases
+{
+    static class DatosConexion
+    {
+        //Federico
+        public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
+
+        //Lautaro
+        //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
+
+        ////Bruno
+        //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
+
+        ////Martin
+        //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
+    }
+}
