3e872767b0c8eaa94f997ae6d4cae6b567033601
Bruno Daniel Toledo Cadario <brunito_toledo@hotmail.com>
2020-08-10 16:04:28 -0300

    cambios

--- TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
+++ TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace TESTWF2020.Clases
 {
     static class DatosConexion
-    {
-        //Esta es la configuracion de cada uno
+    {   
+        //Esta era la config de c/u
         //Federico
         //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
 
@@ -16,9 +16,9 @@ namespace TESTWF2020.Clases
         //public static string connectionString = @"Data Source=DESKTOP-8I3ONNI;Initial Catalog=prueba;Integrated Security=True";
 
         ////Bruno
-        //public static string connectionString = @"Data Source=DESKTOP-0C4GOB8\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
+        public static string connectionString = @"Data Source=DESKTOP-0C4GOB8\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
 
         ////Martin
-        public static string connectionString = @"Data Source=DESKTOP-J8IAG5M\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
+        //public static string connectionString = @"Data Source=DESKTOP-J8IAG5M\SQLEXPRESS;Initial Catalog=prueba;Integrated Security=True";
     }
 }
