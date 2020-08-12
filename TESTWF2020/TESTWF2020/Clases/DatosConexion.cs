3c2e8c6257388ec4eb62c7ae6aedf925c9df11f5
Lautaro Goyenechea <lautaro.goyenechea@hotmail.com>
2020-08-12 16:20:21 -0300

    Corregí la clave de la conectionString

--- TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
+++ TESTWF2020/TESTWF2020/Clases/DatosConexion.cs
@@ -10,7 +10,7 @@ namespace TESTWF2020.Clases
     {
         // este es un comentario para explicar que estas son las cadenas de conexion de cada uno
 
-        public static string connectionString = @"Data Source=186.13.121.219;Initial Catalog=prueba;Persist Security Info=True;User ID=usertest;Password=***********";
+        public static string connectionString = @"Data Source=186.13.121.219;Initial Catalog=prueba;Persist Security Info=True;User ID=usertest;Password=pepito";
 
     }
 }
