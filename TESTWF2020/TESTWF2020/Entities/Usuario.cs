082dcb33e471e601eb99bf87bc45b03db1894d5b
Federico Ayme <federico.ayme@gmail.com>
2020-08-19 16:54:53 -0300

    Agregada clase Entity Usuario

---
+++ TESTWF2020/TESTWF2020/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTWF2020.Entities
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public Perfil Perfil { get; set; }
+    }
+}
