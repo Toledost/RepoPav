84ab6dd953e162f8c74937e638c82f172f0ad09c
Bruno Daniel Toledo Cadario <brunito_toledo@hotmail.com>
2020-08-19 17:58:29 -0300

    agregue logica de UsuarioDao

--- TESTWF2020/TESTWF2020/DataAccessLayer/UsuarioDao.cs
+++ TESTWF2020/TESTWF2020/DataAccessLayer/UsuarioDao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,10 +16,35 @@ namespace TESTWF2020.DataAccessLayer
             string consultaSQL = string.Format ("SELECT u.usuario, u.contraseña, u.fechaAlta, p.idPerfil, p.nombre" +
                 "p.descripcion FROM usuario u JOIN perfil p ON u.idPerfil = p.idPerfil WHERE u.nombre ='{0}' ",nombreUsuario);
 
+            DataTable resultado = null;
+
+            if (resultado.Rows.Count > 0)
+            {
+                return MapToEntity(resultado.Rows[0]);
+            }
+            return null;
         }
         #endregion
+        
         #region Mapeo
-
+        private Usuario MapToEntity(DataRow row)
+        {
+            Usuario usuario = new Usuario
+            {
+                Nombre = row["nombre"].ToString(),
+                Contraseña = row["contraseña"].ToString(),
+                FechaAlta = (DateTime)row["fechaAlta"],
+                Perfil = new Perfil
+                {
+                    Descripcion = row["descripcion"].ToString(),
+                    Nombre = row["nombre"].ToString(),
+                    IdPerfil = (int)row["idPerfil"]
+                }
+            };
+            return usuario; 
+        }
         #endregion
     }
 }
+                
+
