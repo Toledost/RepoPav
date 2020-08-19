adc10252222a5ac8f11b8191370807fa6f93ab1c
Bruno Daniel Toledo Cadario <brunito_toledo@hotmail.com>
2020-08-19 17:47:02 -0300

    agregue logica de UsuarioDao

---
+++ TESTWF2020/TESTWF2020/DataAccessLayer/UsuarioDao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TESTWF2020.Entities;
+
+namespace TESTWF2020.DataAccessLayer
+{
+    class UsuarioDao
+    {
+        #region Metodos Publicos
+        public Usuario GetUsuario(string nombreUsuario)
+        {
+            string consultaSQL = string.Format ("SELECT u.usuario, u.contraseña, u.fechaAlta, p.idPerfil, p.nombre" +
+                "p.descripcion FROM usuario u JOIN perfil p ON u.idPerfil = p.idPerfil WHERE u.nombre ='{0}' ",nombreUsuario);
+
+        }
+        #endregion
+        #region Mapeo
+
+        #endregion
+    }
+}
