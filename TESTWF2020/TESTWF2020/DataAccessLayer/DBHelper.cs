9c529643957877e96af911c4fa9f2cd26d7efac0
Lautaro Goyenechea <lautaro.goyenechea@hotmail.com>
2020-08-19 19:32:30 -0300

    Clase DBHelper

---
+++ TESTWF2020/TESTWF2020/DataAccessLayer/DBHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTWF2020.DataAccessLayer
+{
+    public class DBHelper
+    {
+        private static DBHelper instance = new DBHelper();
+        private string connectionString = @"Data Source=186.13.121.219;Initial Catalog=BDInmobiliariaCasaFeliz;Persist Security Info=True;User ID=usertest;Password=pepito";
+        
+        public static DBHelper GetDBHelper()
+        {
+            if (instance == null)
+            {
+                instance = new DBHelper();
+            }
+            return instance;
+        }
+
+        public DataTable ConsultaSql(string consulta) 
+        {
+            using (var conn = new SqlConnection())
+            {
+                conn.ConnectionString = connectionString;
+                conn.Open();
+
+                using (var command = new SqlCommand(consulta, conn))
+                {
+                    DataTable tabla = new DataTable();
+                    tabla.Load(command.ExecuteReader());
+                    return tabla;
+                }
+            }
+        }
+    }
+}
