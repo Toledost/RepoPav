4290fd52c0fdbae4c0286e4e7ee946b75fb2d3f1
Bruno Daniel Toledo Cadario <brunito_toledo@hotmail.com>
2020-09-12 18:41:48 -0300

    Creacion de clase inmueble y sus prop

---
+++ TESTWF2020/TESTWF2020/Entities/Inmueble.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTWF2020.Entities
+{
+    class Inmueble
+    {
+        public int Id { get; set; }
+        public string TipoInmueble { get; set; }
+        public string Calle { get; set; }
+        public int CalleNumero { get; set; }
+        public int MetrosCuadrados { get; set; }
+        public int Baños { get; set; }
+        public int Habitaciones { get; set; }
+        public int MontoAlq { get; set; }
+        public int MontoVta { get; set; }
+        public string Descripcion { get; set; }
+
+
+
+    }
+}
